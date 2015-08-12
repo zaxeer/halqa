ee358aee940e7c6c5befe8ec58dd178de5a8bd67
Hasham <>
2015-08-12 17:03:03 +0200

    Model added

--- src/main/java/org/ahmadiyya/halqa/software/model/Receipt.java
+++ src/main/java/org/ahmadiyya/halqa/software/model/Receipt.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.annotations.Type;
 
 @Entity
-@Table(name = "Receipt")
+@Table(name = "Receipts")
 public class Receipt {
 	
 	@Id
@@ -45,6 +45,45 @@ public class Receipt {
 	
 	@Column ( name="jalsaSalana" )
 	private BigDecimal jalsaSalana;
+	
+	@Column ( name="tj" )
+	private BigDecimal tj;
+	
+	@Column ( name="wj" )
+	private BigDecimal wj;
+	
+	@Column ( name="bilal" )
+	private BigDecimal bilal;
+	
+	@Column ( name="maryam" )
+	private BigDecimal maryam;
+	
+	@Column ( name="sadqa" )
+	private BigDecimal sadqa;
+	
+	@Column ( name="zakat" )
+	private BigDecimal zakat;
+	
+	@Column ( name="hundertM" )
+	private BigDecimal hundertM;
+	
+	@Column ( name="student" )
+	private BigDecimal student;
+	
+	@Column ( name="mta" )
+	private BigDecimal mta;
+	
+	@Column ( name="lokal" )
+	private BigDecimal lokal;
+	
+	@Column ( name="idCard" )
+	private BigDecimal idCard;
+	
+	@Column ( name="firtana" )
+	private BigDecimal fitrana;
+	
+	@Column ( name="eidFund" )
+	private BigDecimal eidFund;
 
 	public int getId() {
 		return id;
@@ -109,6 +148,110 @@ public class Receipt {
 
 	public void setJalsaSalana(BigDecimal jalsaSalana) {
 		this.jalsaSalana = jalsaSalana;
+	}
+
+	public BigDecimal getTj() {
+		return tj;
+	}
+
+	public void setTj(BigDecimal tj) {
+		this.tj = tj;
+	}
+
+	public BigDecimal getWj() {
+		return wj;
+	}
+
+	public void setWj(BigDecimal wj) {
+		this.wj = wj;
+	}
+
+	public BigDecimal getBilal() {
+		return bilal;
+	}
+
+	public void setBilal(BigDecimal bilal) {
+		this.bilal = bilal;
+	}
+
+	public BigDecimal getMaryam() {
+		return maryam;
+	}
+
+	public void setMaryam(BigDecimal maryam) {
+		this.maryam = maryam;
+	}
+
+	public BigDecimal getSadqa() {
+		return sadqa;
+	}
+
+	public void setSadqa(BigDecimal sadqa) {
+		this.sadqa = sadqa;
+	}
+
+	public BigDecimal getZakat() {
+		return zakat;
+	}
+
+	public void setZakat(BigDecimal zakat) {
+		this.zakat = zakat;
+	}
+
+	public BigDecimal getHundertM() {
+		return hundertM;
+	}
+
+	public void setHundertM(BigDecimal hundertM) {
+		this.hundertM = hundertM;
+	}
+
+	public BigDecimal getStudent() {
+		return student;
+	}
+
+	public void setStudent(BigDecimal student) {
+		this.student = student;
+	}
+
+	public BigDecimal getMta() {
+		return mta;
+	}
+
+	public void setMta(BigDecimal mta) {
+		this.mta = mta;
+	}
+
+	public BigDecimal getLokal() {
+		return lokal;
+	}
+
+	public void setLokal(BigDecimal lokal) {
+		this.lokal = lokal;
+	}
+
+	public BigDecimal getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(BigDecimal idCard) {
+		this.idCard = idCard;
+	}
+
+	public BigDecimal getFitrana() {
+		return fitrana;
+	}
+
+	public void setFitrana(BigDecimal fitrana) {
+		this.fitrana = fitrana;
+	}
+
+	public BigDecimal getEidFund() {
+		return eidFund;
+	}
+
+	public void setEidFund(BigDecimal eidFund) {
+		this.eidFund = eidFund;
 	}
 	
 	
